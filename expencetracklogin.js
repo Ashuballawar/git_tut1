dc7bf3cfaab894791401e07ae15bc933ae2c23d3
Ashuballawar <ashutoshballawar@gmail.com>
2023-01-18 23:30:40 +0530

    expence tracker login fe

---
+++ expencetracklogin.js
@@ -0,0 +1,32 @@
+
+
+loginform=document.getElementById('loginform')
+
+loginform.addEventListener('submit',login)
+
+async function login(e){
+    let response
+    e.preventDefault();
+    User={Email:e.target.Email.value,
+    Password:e.target.Password.value
+    }
+
+    try{
+  response=await axios.post("http://localhost:4000/user/login",User)
+  console.log(response)
+ 
+   if(response.status==201){
+    alert('login successful')
+ }
+  else{
+    throw new Error('login Failed')
+  }
+  
+  }             
+    
+    catch(err){        
+         
+        document.body.innerHTML+=`<div style="color:red;">${err.response.data.msg}</div>`
+    }}
+
+
